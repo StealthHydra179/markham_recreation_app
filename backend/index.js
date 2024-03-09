886cde811e9a115c724e3d95a14454224e7e5d58
Stealth <47306082+StealthHydra179@users.noreply.github.com>
2024-03-09 17:07:06 -0500

    Serverside PG compatibility

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
-// Simple express server
 const express = require('express')
+const { postgress_client: postgres_client } = require('pg')
+require('dotenv').config()
 
 const app = express()
 const port = 3000
 
+const client = new postgres_client();
+await client.connect();
+
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Hello World' })
 })
 
 app.listen(port, () => {
