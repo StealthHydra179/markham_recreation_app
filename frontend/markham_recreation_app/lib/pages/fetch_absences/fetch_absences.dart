d4c17c8383068bb21ad7a4cb3f834158df8b2e46
Stealth <47306082+StealthHydra179@users.noreply.github.com>
2024-03-23 12:03:30 -0400

    Refactor Fetch_absences

--- frontend/markham_recreation_app/lib/pages/fetch_absences/fetch_absences.dart
+++ frontend/markham_recreation_app/lib/pages/fetch_absences/fetch_absences.dart
@@ -2,26 +2,28 @@ library weekly_checklist;
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 import 'dart:convert';
 
 import 'package:markham_recreation_app/drawer.dart' as drawer;
 import 'package:markham_recreation_app/globals.dart' as globals;
 
-import 'absence.dart';
-import 'absence_details.dart';
-import 'edit_absence.dart';
+import 'package:markham_recreation_app/pages/fetch_absences/absence.dart';
+import 'package:markham_recreation_app/pages/fetch_absences/absence_details.dart';
 
+// global variable to store the request to the server (for FutureBuilder)
+late Future<List<Absence>> futureAbsences;
+
+// Fetch absences from the server
 Future<List<Absence>> futureFetchAbsences() async {
   final response = await http.get(
     Uri.parse('${globals.serverUrl}/api/get_absences/${globals.camp_id}'),
   );
 
-  // Create return list
+  // Create List of absences
   List<Absence> absences = [];
 
   if (response.statusCode == 200) {
-    // If server returns an OK response, parse the JSON
+    // If server returns an OK response, parse the JSON and store the absences
     List<dynamic> absencesJson = jsonDecode(response.body);
     absences = absencesJson.map((dynamic json) => Absence.fromJson(json)).toList();
     return absences;
@@ -31,18 +33,19 @@ Future<List<Absence>> futureFetchAbsences() async {
   }
 }
 
+// Format datetime string into a date and a time
 String dateTimeFormatter(String date) {
   // Import form yyyy-mm-ddThh:mm:ss.000Z to mm/dd/yyyy hh:mm
   return '${date.substring(5, 7)}/${date.substring(8, 10)}/${date.substring(0, 4)} ${date.substring(11, 16)}';
 }
 
+// Format datetime string into a date
 String dateFormatter(String date) {
   // Import form yyyy-mm-ddThh:mm:ss.000Z to mm/dd/yyyy
   return '${date.substring(5, 7)}/${date.substring(8, 10)}/${date.substring(0, 4)}';
 }
 
-late Future<List<Absence>> futureAbsences;
-
+// List absences page wrapper
 class FetchAbsences extends StatefulWidget {
   const FetchAbsences({super.key});
 
@@ -50,8 +53,9 @@ class FetchAbsences extends StatefulWidget {
   State<FetchAbsences> createState() => _FetchAbsencesState();
 }
 
+// List absences page content
 class _FetchAbsencesState extends State<FetchAbsences> {
-
+  // Fetch list of absences from the server
   @override
   void initState() {
     super.initState();
@@ -63,15 +67,13 @@ class _FetchAbsencesState extends State<FetchAbsences> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text(
-          'Absences',
-          style: TextStyle(color: globals.secondaryColor) 
-        ),
+        title: const Text('Absences', style: TextStyle(color: globals.secondaryColor)),
         iconTheme: const IconThemeData(color: globals.secondaryColor),
       ),
       drawer: drawer.drawer(context),
       body: Column(
         children: <Widget>[
+          // Display absences in a list view, future builder waits for the server to return the data
           FutureBuilder<List<Absence>>(
             future: futureAbsences,
             builder: (context, snapshot) {
@@ -80,22 +82,22 @@ class _FetchAbsencesState extends State<FetchAbsences> {
                   child: ListView.builder(
                     itemCount: snapshot.data?.length,
                     itemBuilder: (context, index) {
+                      // For each absence, display the camper name, date, and a chevron icon
                       return ListTile(
                         title: Text(snapshot.data![index].camper_name),
                         subtitle: Text(dateFormatter(snapshot.data![index].date)),
-                        trailing: Icon(Icons.chevron_right),
-                        // if not followed up change the background color to a light red
-                        tileColor: snapshot.data![index].followed_up ? null : Color.fromARGB(255, 255, 230, 233),
+                        trailing: const Icon(Icons.chevron_right),
+
+                        // If not followed up change the background color to a light red
+                        tileColor: snapshot.data![index].followed_up ? null : const Color.fromARGB(255, 255, 230, 233),
                         onTap: () {
+                          // Display the absence details page when the absence is tapped
                           Navigator.push(
                             context,
                             MaterialPageRoute(builder: (context) => AbsenceDetails(absence: snapshot.data![index])),
                           ).then((value) {
-                            // if value is not null
-                            // reload the page
-                            print(value);
+                            // Refresh the list of absences after returning from the absence details page
                             futureAbsences = futureFetchAbsences();
-                            
                             setState(() {});
                           });
                         },
@@ -106,10 +108,11 @@ class _FetchAbsencesState extends State<FetchAbsences> {
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
+
               // By default, show a loading spinner.
               return const CircularProgressIndicator();
             },
-            ),
+          ),
         ],
       ),
     );
