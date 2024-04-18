d3314f560a44db0c527589ebe18286c8731266ab
ansonzma <anson.z.ma@gmail.com>
2024-04-17 21:50:18 -0400

    Daily notes changes

--- backend/express/dailyNoteRoutes.js
+++ backend/express/dailyNoteRoutes.js
@@ -1,3 +1,122 @@
 module.exports = function (expressServer, logger, postgresClient, dataSanitization, getPostgresConnected) {
-    logger.warn("dailyNoteRoutes.js not implemented");
-}
+    expressServer.get("/api/get_daily_notes/:camp_id", (req, res) => {
+        let postgresConnected = getPostgresConnected();
+        if (!postgresConnected) {
+            res.status(500).send({ message: "Database not connected" });
+            logger.error("Database not connected");
+            return;
+        }
+        logger.debug(`GET /api/get_daily_notes/:camp_id ${dataSanitization(req.params.camp_id)}`);
+
+        // Rewriting using a join statement
+        const query = `SELECT e.*, u.first_name, u.last_name
+                   FROM daily_note AS e LEFT JOIN app_user AS u ON e.daily_note_upd_by = u.user_id
+                   WHERE camp_id = $1
+                   ORDER BY daily_note_date DESC`;
+        const values = [dataSanitization(req.params.camp_id)];
+
+        postgresClient.query(query, values, (err, result) => {
+            if (err) {
+                logger.error("Get daily notes error: ", err);
+                return;
+            }
+            res.json(result.rows);
+        });
+    });
+
+    expressServer.post("/api/new_daily_note/:camp_id", (req, res) => {
+        let postgresConnected = getPostgresConnected();
+        if (!postgresConnected) {
+            res.status(500).send({ message: "Database not connected" });
+            logger.error("Database not connected");
+            return;
+        }
+        logger.debug(
+            `POST /api/new_daily_note/:camp_id ${dataSanitization(req.params.camp_id)} ${dataSanitization(req.body.daily_note)} ${dataSanitization(req.body.daily_note_date)}`,
+        );
+        logger.warn("TODO do input data validation"); // TODO
+
+        // Add to database
+        const addQuery =
+            "INSERT INTO daily_note (camp_id, daily_note, daily_note_date, daily_note_upd_date, daily_note_upd_by) VALUES ($1, $2, $3, $4, $5)";
+        const addQueryValues = [
+            dataSanitization(req.params.camp_id),
+            dataSanitization(req.body.daily_note),
+            dataSanitization(req.body.daily_note_date),
+            new Date().toISOString(),
+            0, //TODO equie_note_upd_by
+        ];
+
+        postgresClient.query(addQuery, addQueryValues, (err, res) => {
+            if (err) {
+                logger.error("New daily note error: ", err); // TODO send an error to the client // TODO figure out why logger.error gave undefined?
+                return;
+            }
+            logger.info("Added new daily note to database");
+        });
+        res.json(req.body);
+    });
+
+    // TODO sanitize before putting into logger
+    expressServer.post("/api/edit_daily_note/:camp_id", (req, res) => {
+        let postgresConnected = getPostgresConnected();
+        if (!postgresConnected) {
+            res.status(500).send({ message: "Database not connected" });
+            logger.error("Database not connected");
+            return;
+        }
+        logger.debug(
+            `POST /api/edit_daily_note/:camp_id ${dataSanitization(req.params.camp_id)} ${dataSanitization(req.body.daily_note)} ${dataSanitization(req.body.daily_note_date)}`,
+        );
+        logger.warn("TODO do input data validation"); // TODO
+
+        // update specific query
+        const updateQuery =
+            "UPDATE daily_note SET daily_note = $1, daily_note_upd_date = $2, daily_note_upd_by = $3 WHERE daily_note_id = $4";
+        const updateQueryValues = [
+            dataSanitization(req.body.daily_note),
+            new Date().toISOString(),
+            0, //TODO equie_note_upd_by
+            dataSanitization(req.body.daily_note_id),
+        ];
+
+        postgresClient
+            .query(updateQuery, updateQueryValues)
+            .then((res) => {
+                logger.info("Updated daily note in database");
+            })
+            .catch((e) => {
+                logger.error("Edit daily note error: ", e);
+            });
+        res.json(req.body);
+    });
+
+    expressServer.post("/api/delete_daily_note/:camp_id", (req, res) => {
+        let postgresConnected = getPostgresConnected();
+        if (!postgresConnected) {
+            res.status(500).send({ message: "Database not connected" });
+            logger.error("Database not connected");
+            return;
+        }
+        logger.debug(
+            `POST /api/delete_daily_note/:camp_id ${dataSanitization(req.params.camp_id)} ${dataSanitization(req.body.daily_note_id)}`,
+        );
+        logger.warn("TODO do input data validation"); // TODO
+
+        // delete specific query
+        const deleteQuery = "DELETE FROM daily_note WHERE daily_note_id = $1";
+        const deleteQueryValues = [dataSanitization(req.body.daily_note_id)];
+
+        postgresClient
+            .query(deleteQuery, deleteQueryValues)
+            .then((res) => {
+                logger.info("Deleted daily note from database");
+            })
+            .catch((e) => {
+                logger.error("Delete daily note error: ", e);
+            });
+        res.json(req.body);
+    });
+
+    logger.info("dailyNoteRoutes.js loaded");
+};
---
+++ frontend/markham_recreation_app/lib/pages/daily_notes/new_daily_note.dart
@@ -0,0 +1,140 @@
+library weekly_checklist;
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+import 'package:date_field/date_field.dart';
+
+import 'package:markham_recreation_app/globals.dart' as globals;
+
+class NewDailyNote extends StatefulWidget {
+  const NewDailyNote({super.key});
+
+  @override
+  State<NewDailyNote> createState() => _NewDailyNoteState();
+}
+
+class _NewDailyNoteState extends State<NewDailyNote> {
+  DateTime? selectedDate;
+
+  final TextEditingController _notesController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Update checkbox state
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text('New Daily Note', style: TextStyle(color: globals.secondaryColor)),
+        iconTheme: const IconThemeData(color: globals.secondaryColor),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+             Container(
+              margin: const EdgeInsets.all(10),
+              child: SizedBox(
+                child: DateTimeFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Enter Date',
+                  ),
+                  mode: DateTimeFieldPickerMode.date,
+                  firstDate: DateTime.now().add(const Duration(days: -7)),
+                  lastDate: DateTime.now().add(const Duration(days: 7)),
+                  initialPickerDateTime: DateTime.now().add(const Duration(days: 0)),
+                  onChanged: (DateTime? value) {
+                    selectedDate = value;
+                  },
+                ),
+              ),
+            ),
+             Container(
+                margin: const EdgeInsets.all(10),
+                child: SizedBox(
+                  child: TextField(
+                    // TODO make the box expand vertically when new information is added
+                    controller: _notesController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Notes',
+                    ),
+                  ),
+                ),
+              ),
+            const Divider(height: 0),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(10),
+              ),
+              onPressed: () {
+                // TODO input validation
+
+                if (selectedDate == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Please enter a date'),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                  return;
+                }
+
+                if (_notesController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Please enter a note'),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                  return;
+                }
+
+                // Send the checklist to the server
+                Future<http.Response> response = http.post(
+                  Uri.parse('${globals.serverUrl}/api/new_daily_note/${globals.campId}'), //+globals.camp_id.toString()
+                  headers: <String, String>{
+                    'Content-Type': 'application/json; charset=UTF-8',
+                  },
+                  body: jsonEncode(<String, String>{
+                    'daily_note_date': selectedDate.toString(),
+                    'daily_note': _notesController.text,
+                  }),
+                );
+                response.then((http.Response response) {
+                  if (response.statusCode == 200) {
+                    Navigator.pop(context, true); // go back to the previous page and force a refresh
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('New Daily Note Saved'),
+                        duration: Duration(seconds: 3),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Failed to Save New Daily Note'),
+                        duration: Duration(seconds: 3),
+                      ),
+                    );
+                  }
+                }).catchError((error, stackTrace) {
+                  // Runs when the server is down
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Failed to Save Daily Note'),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                });
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
